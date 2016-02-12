4762198dfa1316c806fb2cf290c98934ca877e46
Takenori Takaki <takenory@gmail.com>
2016-02-12 13:00:00 +0900

    * モデル（テーブル）毎の登録処理をメソッドに切り出した * case文による制御を、モデル名から動的に対応するメソッドを呼び出す方式に変更した * 上記に関わるコメントを追記した

--- lib/redmine/demo_data/loader.rb
+++ lib/redmine/demo_data/loader.rb
@@ -8,180 +8,199 @@ module Redmine
           ActiveRecord::Base.transaction do
             YAML::load(ERB.new(File.read(demo_data_yaml_path)).result).each do |yaml|
               yaml.last.each do |table_name, records|
-                model_name = table_name.singularize.camelize
                 records.each do |record|
-                  # recordは配列となっており、record[1]=yamlデータ上の識別子、record[2]=yaml化されたattributesで構成されている
+                  # recordは配列となっており、record[0]=yamlデータ上の識別子、record[1]=yaml化されたattributesで構成されている
                   # そのため、実際に登録したいデータのattributesを、record.lastとして参照している
+                  # (ex)
+                  # testuser1:                  -- record[0]
+                  #   login: 'testuser1'        -- record[1] ==> yaml_attributes
+                  #   password: 'testpassword1' -- record[1]
                   yaml_attributes = record.last
   
-                  # モデル名毎にでもデータを登録する
-                  case model_name
+                  # テーブル毎にデモデータを登録する
+                  # テーブル名に応じて、`entry_テーブル名の単数形` メソッドを呼び出してデータを登録する
+                  # (ex)
+                  # table_name が `users` であれば、 `entry_user(yaml_attributes)` を呼び出し
+                  table_names = %w(users trackers projects members versions files issues)
+                  self.send("entry_#{table_name.singularize}", yaml_attributes) if table_names.include(table_name)
+                end
+              end
+            end
+          end
+        end
+
+        # ユーザーを登録する
+        def entry_user(yaml_attributes)
+          entry_demo_data('User', yaml_attributes, 'login') do |user|
+            user.login = yaml_attributes['login']
+            user.password = yaml_attributes['password']
+            user.password_confirmation = yaml_attributes['password_confirmation']
+            user.admin = false
+            user.generate_password = 0
+            user.must_change_passwd = 0
+            user.register
+            user.activate
+          end
+        end
+
+        # トラッカーを登録する
+        def entry_tracker(yaml_attributes)
+          Tracker.skip_callback(:destroy, :before, :check_integrity)
+          tracker = entry_demo_data('Tracker', yaml_attributes, 'name') do |tracker|
+            tracker.default_status_id = IssueStatus.first.id
+          end
+          # トラッカーにワークフローを設定する
+          print "Updating Tracker #{tracker.name}...."
+          roles = Role.all
+          # YAMLで定義されたワークフローのtransitionsをロードしてHashに変換する
+          transitions_file_path = Dir[File.join(Rails.root, 'plugins', 'redmine_demo_project', 'db', 'transitions.yml')][0]
+          transitions = YAML::load(ERB.new(File.read(transitions_file_path)).result).to_h
+          # YAMLからロードしたワークフローの設定をトラッカーにセットする
+          begin
+            WorkflowTransition.replace_transitions(tracker, roles, transitions)
+            puts 'SUCCESS!'
+          rescue => e
+            puts "FAILD! : #{e.message}"
+            raise e
+          end
+        end
+
+        # プロジェクトを登録する
+        def entry_project(yaml_attributes)
+          tracker_names = yaml_attributes.delete('tracker_names')
+          entry_demo_data('Project', yaml_attributes, 'identifier') do |project|
+            trackers = Tracker.where('name IN (?)', tracker_names)
+            project.trackers = trackers
+          end
+        end
+
+        # メンバーを登録する
+        def entry_member(yaml_attributes)
+          user_login = yaml_attributes.delete('user_login')
+          user = User.find_by login: user_login
+          project_identifier = yaml_attributes.delete('project_identifier')
+          project = Project.find_by identifier: project_identifier
   
-                  when "User"
-                    entry_demo_data(model_name, yaml_attributes, 'login') do |user|
-                      user.login = yaml_attributes['login']
-                      user.password = yaml_attributes['password']
-                      user.password_confirmation = yaml_attributes['password_confirmation']
-                      user.admin = false
-                      user.generate_password = 0
-                      user.must_change_passwd = 0
-                      user.register
-                      user.activate
-                    end
-  
-                  when "Tracker"
-                    Tracker.skip_callback(:destroy, :before, :check_integrity)
-                    tracker = entry_demo_data(model_name, yaml_attributes, 'name') do |tracker|
-                      tracker.default_status_id = IssueStatus.first.id
-                    end
-                    # トラッカーにワークフローを設定する
-                    print "Updating Tracker #{tracker.name}...."
-                    roles = Role.all
-                    # YAMLで定義されたワークフローのtransitionsをロードしてHashに変換する
-                    transitions_file_path = Dir[File.join(Rails.root, 'plugins', 'redmine_demo_project', 'db', 'transitions.yml')][0]
-                    transitions = YAML::load(ERB.new(File.read(transitions_file_path)).result).to_h
-                    # YAMLからロードしたワークフローの設定をトラッカーにセットする
-                    begin
-                      WorkflowTransition.replace_transitions(tracker, roles, transitions)
-                      puts 'SUCCESS!'
-                    rescue => e
-                      puts "FAILD! : #{e.message}"
-                      raise e
-                    end
-  
-                  when "Project"
-                    tracker_names = yaml_attributes.delete('tracker_names')
-                    entry_demo_data(model_name, yaml_attributes, 'identifier') do |project|
-                      trackers = Tracker.where('name IN (?)', tracker_names)
-                      project.trackers = trackers
-                    end
-  
-                  when "Member"
-                    user_login = yaml_attributes.delete('user_login')
-                    user = User.find_by login: user_login
-                    project_identifier = yaml_attributes.delete('project_identifier')
-                    project = Project.find_by identifier: project_identifier
-  
-                    print "Adding Member #{user.login} to --> #{project.name}..."
-                    member = Member.new(project: project, user_id: user.id)
-                    admin = User.active.find_by(login: "admin") || User.active.where(admin: true).first
-                    member.set_editable_role_ids(Role.givable.pluck(:id).collect {|e| e.to_s}, admin)
-                    begin
-                      member.save!
-                      puts 'SUCCESS!'
-                    rescue => e
-                      puts "FAILD! : #{e.message}"
-                      raise e
-                    end
-  
-                  when "Version"
-                    project_identifier = yaml_attributes.delete('project_identifier')
-                    project = Project.find_by identifier: project_identifier
-                    yaml_attributes['project_id'] = project.id
-                    entry_demo_data(model_name, yaml_attributes, ['name', 'project_id']) do |version|
-                      project = Project.find_by identifier: project_identifier
-                      version.project = project
-                    end
+          print "Adding Member #{user.login} to --> #{project.name}..."
+          member = Member.new(project: project, user_id: user.id)
+          admin = User.active.find_by(login: "admin") || User.active.where(admin: true).first
+          member.set_editable_role_ids(Role.givable.pluck(:id).collect {|e| e.to_s}, admin)
+          begin
+            member.save!
+            puts 'SUCCESS!'
+          rescue => e
+            puts "FAILD! : #{e.message}"
+            raise e
+          end
+        end
 
-                  when "File"
-                    project_identifier = yaml_attributes.delete('project_identifier')
-                    project = Project.find_by identifier: project_identifier
-                    version_name = yaml_attributes.delete('version_name')
-                    attachment_filename = yaml_attributes.delete('attachment_filename')
-                    attachment_description = yaml_attributes.delete('attachment_description')
-                    author_user_login = yaml_attributes.delete('author_user_login')
-                    if version = Version.find_by(project_id: project.id, name: version_name)
-                      # 添付ファイルのダミーデータを設定
-                      if attachment_filename && author_user_login
-                        author = User.find_by login: author_user_login
-                        attach_file!(version, attachment_filename, author, attachment_description)
-                      end
-                    end
-
-                  when "Issue"
-                    project_identifier = yaml_attributes.delete('project_identifier')
-                    assigned_user_login = yaml_attributes.delete('assigned_user_login')
-                    assigned_user = User.find_by login: assigned_user_login
-                    fixed_version_name = yaml_attributes.delete('fixed_version_name')
-                    issue_status_name = yaml_attributes.delete('status_name')
-                    tracker_name = yaml_attributes.delete('tracker_name')
-                    start_date = yaml_attributes.delete('start_date')
-                    end_date = yaml_attributes.delete('end_date')
-                    attachment_filename = yaml_attributes.delete('attachment_filename')
-                    attachment_description = yaml_attributes.delete('attachment_description')
-
-                    # チケットを新規作成する
-                    issue = entry_demo_data(model_name, yaml_attributes, ['subject', 'fixed_version_id'], false) do |issue|
-                      project = Project.find_by identifier: project_identifier
-                      fixed_version = Version.find_by name: fixed_version_name
-                      # issue_status = IssueStatus.find_by name: issue_status_name
-                      issue_status = IssueStatus.find_by name: '新規'
-                      tracker = Tracker.find_by name: tracker_name
-                      issue.project = project
-                      issue.author = assigned_user
-                      issue.assigned_to = assigned_user
-                      issue.fixed_version = fixed_version
-                      issue.status = issue_status
-                      #issue.tracker ||= issue.project.trackers.first
-                      issue.tracker = tracker
-                    end
-
-                    # ステータスに応じてチケットを編集-->更新する
-                    issue_status = IssueStatus.find_by name: issue_status_name
-                    
-                    if issue_status_name == "進行中" || issue_status_name == "終了"
-                      print "Updating Issue #{issue.subject} status -> 進行中..."
-                      issue.init_journal(assigned_user, '作業に着手しました。')
-                      journal = issue.current_journal
-                      journal.created_on = start_date
-                      issue_status = IssueStatus.find_by name: '進行中'
-                      issue.status = issue_status
-                      issue.start_date = start_date
-                      issue.done_ratio = rand(1..8) * 10 # 進捗率 = 10% 〜 80%
-                      begin
-                        issue.save!(validate: false)
-                        puts 'SUCCESS!'
-                      rescue => e
-                        puts "FAILD! : #{e.message}"
-                        raise e
-                      end
-                    end
-
-                    if issue_status_name == "終了"
-                      print "Updating Issue #{issue.subject} status -> 終了..."
-                      # issue.reload  # MEMO: issue.reloadだと進行中で設定したjournalが上書きされるので、もう一度findする
-                      issue = Issue.find(issue.id)
-                      issue.init_journal(assigned_user, '作業が完了しました。')
-                      journal = issue.current_journal
-                      journal.created_on = end_date if end_date.present?
-                      issue_status = IssueStatus.find_by name: '終了'
-                      issue.status = issue_status
-                      # :issue.update_done_ratio_from_issue_status # MEMO: issue_statusにdone_ratioが設定されていないと有効にならない...
-                      issue.done_ratio = 100 # 進捗率 = 100%
-
-                      # 添付ファイルのダミーデータを設定
-                      if attachment_filename.present?
-                        attach_file!(issue, attachment_filename, assigned_user, attachment_description)
-                      end
-
-                      begin
-                        issue.save!(validate: false)
-                        puts 'SUCCESS!'
-                      rescue => e
-                        puts "FAILD! : #{e.message}"
-                        raise e
-                      end
-                    end
-
-                  end # end of case model_name
-                end # end of records.each
-              end # end of yaml.last.each
-            end # end of YAML.load(....).each
-          end # end of ActiveRecord::Base.transaction
+        # バージョンを登録する
+        def entry_version(yaml_attributes)
+          project_identifier = yaml_attributes.delete('project_identifier')
+          project = Project.find_by identifier: project_identifier
+          yaml_attributes['project_id'] = project.id
+          entry_demo_data('Version', yaml_attributes, ['name', 'project_id']) do |version|
+            project = Project.find_by identifier: project_identifier
+            version.project = project
+          end
         end
   
+        # ファイルを登録する
+        def entry_file(yaml_attributes)
+          project_identifier = yaml_attributes.delete('project_identifier')
+          project = Project.find_by identifier: project_identifier
+          version_name = yaml_attributes.delete('version_name')
+          attachment_filename = yaml_attributes.delete('attachment_filename')
+          attachment_description = yaml_attributes.delete('attachment_description')
+          author_user_login = yaml_attributes.delete('author_user_login')
+          if version = Version.find_by(project_id: project.id, name: version_name)
+            # 添付ファイルのダミーデータを設定
+            if attachment_filename && author_user_login
+              author = User.find_by login: author_user_login
+              attach_file!(version, attachment_filename, author, attachment_description)
+            end
+          end
+        end
+
+        # チケットを登録する
+        def entry_issue(yaml_attributes)
+          project_identifier = yaml_attributes.delete('project_identifier')
+          assigned_user_login = yaml_attributes.delete('assigned_user_login')
+          assigned_user = User.find_by login: assigned_user_login
+          fixed_version_name = yaml_attributes.delete('fixed_version_name')
+          issue_status_name = yaml_attributes.delete('status_name')
+          tracker_name = yaml_attributes.delete('tracker_name')
+          start_date = yaml_attributes.delete('start_date')
+          end_date = yaml_attributes.delete('end_date')
+          attachment_filename = yaml_attributes.delete('attachment_filename')
+          attachment_description = yaml_attributes.delete('attachment_description')
+
+          # チケットを新規作成する
+          issue = entry_demo_data('Issue', yaml_attributes, ['subject', 'fixed_version_id'], false) do |issue|
+            project = Project.find_by identifier: project_identifier
+            fixed_version = Version.find_by name: fixed_version_name
+            # issue_status = IssueStatus.find_by name: issue_status_name
+            issue_status = IssueStatus.find_by name: '新規'
+            tracker = Tracker.find_by name: tracker_name
+            issue.project = project
+            issue.author = assigned_user
+            issue.assigned_to = assigned_user
+            issue.fixed_version = fixed_version
+            issue.status = issue_status
+            #issue.tracker ||= issue.project.trackers.first
+            issue.tracker = tracker
+          end
+
+          # ステータスに応じてチケットを編集-->更新する
+          issue_status = IssueStatus.find_by name: issue_status_name
+          
+          if issue_status_name == "進行中" || issue_status_name == "終了"
+            print "Updating Issue #{issue.subject} status -> 進行中..."
+            issue.init_journal(assigned_user, '作業に着手しました。')
+            journal = issue.current_journal
+            journal.created_on = start_date
+            issue_status = IssueStatus.find_by name: '進行中'
+            issue.status = issue_status
+            issue.start_date = start_date
+            issue.done_ratio = rand(1..8) * 10 # 進捗率 = 10% 〜 80%
+            begin
+              issue.save!(validate: false)
+              puts 'SUCCESS!'
+            rescue => e
+              puts "FAILD! : #{e.message}"
+              raise e
+            end
+          end
+
+          if issue_status_name == "終了"
+            print "Updating Issue #{issue.subject} status -> 終了..."
+            # issue.reload  # MEMO: issue.reloadだと進行中で設定したjournalが上書きされるので、もう一度findする
+            issue = Issue.find(issue.id)
+            issue.init_journal(assigned_user, '作業が完了しました。')
+            journal = issue.current_journal
+            journal.created_on = end_date if end_date.present?
+            issue_status = IssueStatus.find_by name: '終了'
+            issue.status = issue_status
+            # :issue.update_done_ratio_from_issue_status # MEMO: issue_statusにdone_ratioが設定されていないと有効にならない...
+            issue.done_ratio = 100 # 進捗率 = 100%
+
+            # 添付ファイルのダミーデータを設定
+            if attachment_filename.present?
+              attach_file!(issue, attachment_filename, assigned_user, attachment_description)
+            end
+
+            begin
+              issue.save!(validate: false)
+              puts 'SUCCESS!'
+            rescue => e
+              puts "FAILD! : #{e.message}"
+              raise e
+            end
+          end
+        end
+
         #
-        # デモデータを登録するメソッド
+        # デモデータを登録する
         #
         def entry_demo_data(model_name, yaml_attributes, find_keys, validate = true)
           model_klass = model_name.constantize
